6c3a698e77ef151e7bb609a2baf8db48a2506d18
Woonomics <gm38921@gmail.com>
2019-03-06 21:54:09 +0100

    Whitebox updates

--- whitebox/wb.js
+++ whitebox/wb.js
@@ -3,18 +3,21 @@ $( document ).ready(function() {
     $("#inputbar").on('input',function() {
         switch ($("#inputbar").val()){
             case "cazzo":
-            $("#outputbar").html("cazzo")
+            $("#outputbar").html("Non dire parolacce.")
             break;
-            case "pene":
-            $("#outputbar").html("pene")
-            break;
-            case "tuamamma":
+            case "riccardo":
             $("#outputbar").html("<img src=\"https://i.imgur.com/8Ix2n1A.jpg\">")
             break;
-
+            case "Riccardo":
+            $("#outputbar").html("<img src=\"https://i.imgur.com/8Ix2n1A.jpg\">")
+            break;
+            case "Federica":
+            $("#outputbar").html("<img src=\"https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678087-heart-512.png\" width=\"40%\" height=\"40%\">");
+            break;
             default:
-            $("#outputbar").html("");
-        }  
+            $("#outputbar").html("")
+            break;
+        }
             
     });
 });
