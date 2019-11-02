c525c2a6560b9c107e36085ead8c223fa7fe1e9e
Woonomics <gm38921@gmail.com>
2019-11-02 20:33:50 +0100

    Changed game UI structure, added Upgrades

--- projects/potato2/main.js
+++ projects/potato2/main.js
@@ -10,8 +10,8 @@ side_menu_button.addEventListener('click', _ => {
 
 //pages variables
 const make_potato_page = document.getElementById("make_page");
-const potato_market_page = document.getElementById("potato_market_page");
 const potato_makers_page = document.getElementById("potato_makers_page");
+const upgrades_page = document.getElementById("upgrades_page");
 const statistics_page = document.getElementById("statistics_page");
 const settings_page = document.getElementById("settings_page");
 const howtoplay_page = document.getElementById("howtoplay_page");
@@ -30,18 +30,19 @@ function show_make_page() {
 }
 
 
-function show_potato_market_page() {
-
-    current_page.style.display = "none";
-    current_page = potato_market_page
-    current_page.style.display = "block";
-
-}
-
 function show_potato_makers_page() {
 
     current_page.style.display = "none";
     current_page = potato_makers_page
+    current_page.style.display = "block";
+
+}
+
+
+function show_upgrades_page() {
+
+    current_page.style.display = "none";
+    current_page = upgrades_page
     current_page.style.display = "block";
 
 }
@@ -115,11 +116,12 @@ var money_display = document.getElementById("money_count")
 
 
 //this variable defines the power of one click
-var potato_power = 1
+var power = 1
+var power_display = document.getElementById("power")
 
 //game checks
+
 potato_count_display[0].innerHTML = potato_count
-potato_count_display[1].innerHTML = potato_count
 
 potato_tot_display[0].innerHTML = potato_tot
 potato_tot_display[1].innerHTML = potato_tot
@@ -127,18 +129,23 @@ potato_tot_display[1].innerHTML = potato_tot
 potato_price_display.innerHTML = potato_price
 money_display.innerHTML = money
 
+power_display.innerHTML = power
+
+
 
 //main loop of the game, checks and updates everything
 function game_update() {
 
     potato_count_display[0].innerHTML = potato_count
-    potato_count_display[1].innerHTML = potato_count
+
 
     potato_tot_display[0].innerHTML = potato_tot
     potato_tot_display[1].innerHTML = potato_tot
 
     potato_price_display.innerHTML = potato_price
     money_display.innerHTML = money
+
+    power_display.innerHTML = power
 
 
 }
@@ -147,8 +154,8 @@ setInterval(game_update, 100)
 
 function make() {
 
-    potato_count += 1 * potato_power
-    potato_tot += 1 * potato_power
+    potato_count += 1 * power
+    potato_tot += 1 * power
     flash_make()
 
 }
@@ -162,16 +169,36 @@ function flash_make() {
         document.getElementById("main_counter").style.color = "olive";
         document.getElementById("content").style.background = "black";
 
+        document.getElementById("sell_potato").style.background = "black";
+        document.getElementById("price_counter").style.background = "black";
+
+        document.getElementsByClassName("tot_counter")[0].style.color = "olive";
+
+        document.getElementById("power_counter").style.background = "black";
+        document.getElementById("make_power").style.background = "black";
+
+
+
         potato_count_display[0].style.color = "yellow";
+        potato_tot_display[0].style.color = "yellow";
 
     }
     function flash_out() {
+
         potato_count_display[0].style.color = "white";
+        potato_tot_display[0].style.color = "white";
+
         document.getElementById("main_counter").style.color = "white";
         document.getElementById("content").style.background = "var(--bg-color)";
 
+        document.getElementById("sell_potato").style.background = "linear-gradient(270deg, rgba(38,38,38,1) 0%, rgba(12,55,0,0.5) 50%, rgba(38,38,38,1) 100%)";
+        document.getElementById("price_counter").style.background = "linear-gradient(270deg, rgba(38,38,38,1) 0%, rgba(5, 24, 0, 0.5) 50%, rgba(38,38,38,1) 100%)";
 
+        document.getElementsByClassName("tot_counter")[0].style.color = "white";
+   
 
+        document.getElementById("power_counter").style.background = "linear-gradient(270deg, rgba(38,38,38,0.5) 0%, rgba(54,0,0,0.5) 50%, rgba(38,38,38,0.5) 100%)";
+        document.getElementById("make_power").style.background = "linear-gradient(270deg, rgba(38,38,38,0.5) 0%, rgba(74,0,0,0.5) 50%, rgba(38,38,38,0.5) 100%)";
 
     }
 
