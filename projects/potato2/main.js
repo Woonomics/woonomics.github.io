87d24f64cab05351fe0ab603edef82fd52d1cf70
Woonomics <gm38921@gmail.com>
2019-11-06 13:50:00 +0100

    Implemented potato power

--- projects/potato2/main.js
+++ projects/potato2/main.js
@@ -124,7 +124,6 @@ var power_display = document.getElementById("power")
 potato_count_display[0].innerHTML = potato_count
 
 potato_tot_display[0].innerHTML = potato_tot
-potato_tot_display[1].innerHTML = potato_tot
 
 potato_price_display.innerHTML = potato_price
 money_display.innerHTML = money
@@ -140,12 +139,14 @@ function game_update() {
 
 
     potato_tot_display[0].innerHTML = potato_tot
-    potato_tot_display[1].innerHTML = potato_tot
 
-    potato_price_display.innerHTML = potato_price
+
     money_display.innerHTML = money
 
     power_display.innerHTML = power
+
+    price_change()
+    potato_price_display.innerHTML = potato_price
 
 
 }
@@ -160,6 +161,85 @@ function make() {
 
 }
 
+function sell() {
+
+    if (potato_count > 0){
+
+        money += potato_count*potato_price
+        potato_count = 0
+        flash_sell()
+
+        return
+
+
+
+    }else{
+        
+        flash_sell_no()
+        return
+
+    }
+    
+}
+
+var prices = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 5]
+function price_change() {
+
+    var choice = prices[Math.floor(Math.random()*prices.length)];
+
+    switch(choice){
+        case 0:
+                break;
+        case 1:
+            potato_price = 1;
+
+            break;
+        case 2:
+            potato_price = 2;
+
+            break;
+        case 3:
+            potato_price = 3;
+
+            break;
+        case 4:
+            potato_price = 4;
+
+            break;
+        case 5:
+            potato_price = 5;
+
+            break;
+    }
+    
+}
+
+function increase_power() {
+
+    power += 1
+
+}
+
+
+function decrease_power() {
+
+        if (power > 1){
+            power -= 1
+            power_display.innerHTML = power
+        }
+        if (power > 10){
+            power -= 5
+            power_display.innerHTML = power
+        }
+
+}
+
+setInterval(decrease_power, 500)
+
+
+
+
+
 //////////////////////////////////////////////////--GAME FLASHES
 
 function flash_make() {
@@ -172,21 +252,22 @@ function flash_make() {
         document.getElementById("sell_potato").style.background = "black";
         document.getElementById("price_counter").style.background = "black";
 
-        document.getElementsByClassName("tot_counter")[0].style.color = "olive";
-
         document.getElementById("power_counter").style.background = "black";
         document.getElementById("make_power").style.background = "black";
 
 
 
         potato_count_display[0].style.color = "yellow";
-        potato_tot_display[0].style.color = "yellow";
+        money_display.style.color = "olive";
+        
 
     }
     function flash_out() {
 
         potato_count_display[0].style.color = "white";
         potato_tot_display[0].style.color = "white";
+        money_display.style.color = "white";
+        
 
         document.getElementById("main_counter").style.color = "white";
         document.getElementById("content").style.background = "var(--bg-color)";
@@ -194,9 +275,99 @@ function flash_make() {
         document.getElementById("sell_potato").style.background = "linear-gradient(270deg, rgba(38,38,38,1) 0%, rgba(12,55,0,0.5) 50%, rgba(38,38,38,1) 100%)";
         document.getElementById("price_counter").style.background = "linear-gradient(270deg, rgba(38,38,38,1) 0%, rgba(5, 24, 0, 0.5) 50%, rgba(38,38,38,1) 100%)";
 
-        document.getElementsByClassName("tot_counter")[0].style.color = "white";
    
+        document.getElementById("power_counter").style.background = "linear-gradient(270deg, rgba(38,38,38,0.5) 0%, rgba(54,0,0,0.5) 50%, rgba(38,38,38,0.5) 100%)";
+        document.getElementById("make_power").style.background = "linear-gradient(270deg, rgba(38,38,38,0.5) 0%, rgba(74,0,0,0.5) 50%, rgba(38,38,38,0.5) 100%)";
 
+    }
+
+    flash_in()
+    setTimeout(flash_out, 100)
+
+}
+
+function flash_sell() {
+
+    function flash_in() {
+
+        document.getElementById("main_counter").style.color = "DarkGreen";
+        document.getElementById("content").style.background = "DarkGreen";
+
+        document.getElementById("sell_potato").style.background = "DarkGreen";
+
+        document.getElementById("sell_potato").style.color = "black";
+        document.getElementById("price_counter").style.background = "DarkGreen";
+        document.getElementById("make_potato").style.background = "DarkGreen";
+
+        document.getElementById("power_counter").style.background = "DarkGreen";
+        document.getElementById("make_power").style.background = "DarkGreen";
+
+
+
+        potato_count_display[0].style.color = "red";
+        money_display.style.color = "yellow";
+
+
+    }
+    function flash_out() {
+
+        potato_count_display[0].style.color = "white";
+        money_display.style.color = "white";
+
+        document.getElementById("main_counter").style.color = "white";
+        document.getElementById("content").style.background = "var(--bg-color)";
+
+        document.getElementById("sell_potato").style.color = "white";
+        document.getElementById("sell_potato").style.background = "linear-gradient(270deg, rgba(38,38,38,1) 0%, rgba(12,55,0,0.5) 50%, rgba(38,38,38,1) 100%)";
+        document.getElementById("price_counter").style.background = "linear-gradient(270deg, rgba(38,38,38,1) 0%, rgba(5, 24, 0, 0.5) 50%, rgba(38,38,38,1) 100%)";
+
+
+        document.getElementById("make_potato").style.background = "linear-gradient(270deg, rgba(38,38,38,1) 0%, rgba(55,53,0,0.5) 50%, rgba(38,38,38,1) 100%)";
+        
+        document.getElementById("power_counter").style.background = "linear-gradient(270deg, rgba(38,38,38,0.5) 0%, rgba(54,0,0,0.5) 50%, rgba(38,38,38,0.5) 100%)";
+        document.getElementById("make_power").style.background = "linear-gradient(270deg, rgba(38,38,38,0.5) 0%, rgba(74,0,0,0.5) 50%, rgba(38,38,38,0.5) 100%)";
+
+    }
+
+    flash_in()
+    setTimeout(flash_out, 100)
+
+}
+
+function flash_sell_no() {
+
+    function flash_in() {
+
+        document.getElementById("main_counter").style.color = "grey";
+        document.getElementById("content").style.background = "grey";
+
+        document.getElementById("make_potato").style.background = "grey";
+
+        document.getElementById("sell_potato").style.background = "red";
+        document.getElementById("price_counter").style.background = "grey";
+
+        document.getElementById("power_counter").style.background = "grey";
+        document.getElementById("make_power").style.background = "grey";
+
+        potato_count_display[0].style.color = "grey";
+        money_display.style.color = "grey"
+
+
+
+    }
+    function flash_out() {
+
+        potato_count_display[0].style.color = "white";
+        money_display.style.color = "white";
+
+        document.getElementById("main_counter").style.color = "white";
+        document.getElementById("content").style.background = "var(--bg-color)";
+
+        document.getElementById("sell_potato").style.background = "linear-gradient(270deg, rgba(38,38,38,1) 0%, rgba(12,55,0,0.5) 50%, rgba(38,38,38,1) 100%)";
+        document.getElementById("price_counter").style.background = "linear-gradient(270deg, rgba(38,38,38,1) 0%, rgba(5, 24, 0, 0.5) 50%, rgba(38,38,38,1) 100%)";
+
+        document.getElementById("make_potato").style.background = "linear-gradient(270deg, rgba(38,38,38,1) 0%, rgba(55,53,0,0.5) 50%, rgba(38,38,38,1) 100%)";
+        
         document.getElementById("power_counter").style.background = "linear-gradient(270deg, rgba(38,38,38,0.5) 0%, rgba(54,0,0,0.5) 50%, rgba(38,38,38,0.5) 100%)";
         document.getElementById("make_power").style.background = "linear-gradient(270deg, rgba(38,38,38,0.5) 0%, rgba(74,0,0,0.5) 50%, rgba(38,38,38,0.5) 100%)";
 
